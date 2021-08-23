accba0d2cacad090c4da23929201036db025f4d0
Michael Z <flashman123@fastmail.fm>
2021-08-23 12:11:06 +0100

    request params added

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-console.log("hello world");
+
+const express = require("express");
+require("dotenv").config();
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.get("/", (req, res) => {
+    res.status(200).send("hello world again");
+});
+
+app.get("/about", (req, res) => {
+    res.status(200).send("This is the about route");
+});
+
+app.get("/users/:username", (req, res) => {
+    res.status(200).send(`You requested information about ${req.params.username}`)
+});
+
+app.listen(port, () => {
+    console.log("App is online");
+});
